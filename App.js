7898389375a19c578d3399a431e6935956da0802
Federico Fernandez <fedegas08@gmail.com>
2021-09-19 21:27:14 -0300

    Add testing

--- App.js
+++ App.js
@@ -1,111 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const App = () => {
+  const [squareColor, setSquareColor] = useState(false);
 
-const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+      <View style={styles.container}>
         <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+          style={squareColor ? styles.activeSquare : styles.inactiveSquare}
+          testID="square"
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setSquareColor(!squareColor)}
+          activeOpacity={0.6}
+          testID={'button'}>
+          <Text style={styles.text}>Press Me</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  button: {
+    width: '80%',
+    height: '10%',
+    backgroundColor: 'grey',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  text: {
+    fontSize: 20,
+    color: 'white',
   },
-  highlight: {
-    fontWeight: '700',
+  activeSquare: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'yellow',
+  },
+  inactiveSquare: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'black',
   },
 });
 
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,10 +5,30 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import TestRenderer, {act} from 'react-test-renderer';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+describe('App', () => {
+  const wrapper = TestRenderer.create(<App />);
+  const instance = wrapper.root;
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+  it('renders correctly', () => {
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  describe('when button is not clicked', () => {
+    let square = instance.findByProps({testID: 'square'});
+    expect(square.props.style.backgroundColor).toBe('black');
+  });
+
+  describe('when button is clicked', () => {
+    it('setSquareColor() is called with opposite value', () => {
+      let square = instance.findByProps({testID: 'square'});
+      const button = instance.findByProps({testID: 'button'});
+      act(() => {
+        button.props.onPress();
+      });
+      square = instance.findByProps({testID: 'square'});
+      expect(square.props.style.backgroundColor).toBe('yellow');
+    });
+  });
 });
